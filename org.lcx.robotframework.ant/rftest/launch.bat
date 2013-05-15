feedc6bcc0ba6236f93c5af52c4c252c6c13c625
laurent.carbonnaux <laurent.carbonnaux@f5b72ca0-b16b-b58a-6bd8-96ed0cd42681>
2013-05-15 20:37:01 +0000

    rfant 0.7

--- org.lcx.robotframework.ant/rftest/launch.bat
+++ org.lcx.robotframework.ant/rftest/launch.bat
@@ -4,6 +4,6 @@ call startselenium.bat
 
 :: launch the test
 set JAVA_OPTS=-Xmx512m
-set ROBOTFRAMEWORK=.\lib\robotframework-2.5.6.jar
+set ROBOTFRAMEWORK=.\lib\robotframework-2.7.7.jar
 set RF_OPTS=--debugfile jybot.log --loglevel TRACE --outputdir results
 java %JAVA_OPTS%  -jar %ROBOTFRAMEWORK% %RF_OPTS% %*
